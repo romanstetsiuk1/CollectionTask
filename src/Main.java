588e74118d9acf7b6839f12fd7c1bf1220e75744
Roman Stetsiuk <romansteciuk021@gmail.com>
2018-10-28 09:58:31 +0100

    Create method by add Math.random value to list

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,31 @@
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Random;
+
 public class Main {
+
+//      Task 1: Random float list.
+    public static void randomFloatList() {
+        List<Float> floatList = new LinkedList<Float>();
+        Random random = new Random();
+        while (Math.random() > 0.01) {
+            Float value = random.nextFloat() * 100.0f;
+            floatList.add(value);
+        }
+        floatList
+                .forEach(value -> System.out.println("Result: " + Math.PI * value));
+    }
+
+    public static void main(String[] args) {
+
+        randomFloatList();
+
+
+
+
+
+
+    }
+
 }
